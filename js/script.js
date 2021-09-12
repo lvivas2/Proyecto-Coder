fa6cdfbb9e62479439bdad9ca913beaf69b933d0
lvivas <vivasleonardo4@gmail.com>
2021-09-12 20:22:20 -0300

    Nueva pantalla de carga, aplicada a todas las paginas

---
+++ js/script.js
@@ -0,0 +1,6 @@
+window.addEventListener('load', () =>{
+    const container_loader = document.querySelector('.container_loader')
+    container_loader.style.opacity = 0
+    container_loader.style.visibility = 'hidden'
+
+})
